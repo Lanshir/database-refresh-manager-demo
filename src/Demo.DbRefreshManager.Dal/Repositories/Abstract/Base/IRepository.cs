90a6cfd233ea010c58f2f00b142f718481d35a5a
Alexander Bondarenko <lanshir@ya.ru>
2026-04-16 13:52:42 +0300

    Cleanup after clean arch migration

--- src/Demo.DbRefreshManager.Dal/Repositories/Abstract/Base/IRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Demo.DbRefreshManager.Dal.Repositories.Abstract.Base;
-
-/// <summary>
-/// Интерфейс базового репозитория.
-/// </summary>
-/// <typeparam name="TEntity">Тип сущности репозитория.</typeparam>
-public interface IRepository<TEntity> where TEntity : class { }
